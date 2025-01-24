7c8b39c7927fe85efd794b2155d0fe295ab9122b
lilyium <150597285+lilyium@users.noreply.github.com>
2025-01-24 14:43:26 +0800

    Complete Level 1

--- src/main/java/Baron.java
+++ src/main/java/Baron.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 public class Baron {
-    private static String hello = "____________________________________________________________\n" +
+    private static String horizontalLine = "____________________________________________________________";
+    private static String hello = horizontalLine + "\n" +
             "Hello! I'm Baron.\n" +
             "What can I do for you?\n" +
-            "____________________________________________________________";
+            horizontalLine;
 
     private static String bye = "Bye. Hope to see you again soon!\n" +
-            "____________________________________________________________";
+            horizontalLine;
 
 
     public static void main(String[] args) {
         System.out.println(hello);
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(horizontalLine + "\n" + input + "\n" + horizontalLine);
+            input = s.nextLine();
+        }
+
         System.out.println(bye);
     }
 }
