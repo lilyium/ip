ec069c0fe6d26477aeab0aea19ca977f51e5b7c0
lilyium <150597285+lilyium@users.noreply.github.com>
2025-01-24 15:13:46 +0800

    Complete Level 3

--- src/main/java/Baron.java
+++ src/main/java/Baron.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 
 public class Baron {
     private static String horizontalLine = "____________________________________________________________";
-    private static String hello = horizontalLine + "\n" +
-            "Hello! I'm Baron.\n" +
+    private static String hello = horizontalLine +
+            "\nHello! I'm Baron.\n" +
             "What can I do for you?\n" +
             horizontalLine;
-
-    private static String bye = "Bye. Hope to see you again soon!\n" +
+    private static String bye = horizontalLine +
+            "\nBye. Hope to see you again soon!\n" +
             horizontalLine;
-
 
     public static void main(String[] args) {
         System.out.println(hello);
@@ -19,19 +18,22 @@ public class Baron {
         String input = s.nextLine();
 
         while (!input.equals("bye")) {
+            System.out.println(horizontalLine);
             if (input.equals("list")) {
-                System.out.println(horizontalLine);
                 for (int i = 0; i < taskList.size(); i++) {
                     System.out.println(i + 1 + ". " + taskList.get(i));
                 }
-                System.out.println(horizontalLine);
+            } else if (input.startsWith("mark ")) {
+                taskList.get(Integer.parseInt(input.substring(5)) - 1).mark();
+            } else if (input.startsWith("unmark ")) {
+                taskList.get(Integer.parseInt(input.substring(7)) - 1).unmark();
             } else {
                 taskList.add(new Task(input));
-                System.out.println(horizontalLine + "\n added: " + input + "\n" + horizontalLine);
+                System.out.println("added: " + input);
             }
+            System.out.println(horizontalLine);
             input = s.nextLine();
         }
-
         System.out.println(bye);
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,28 @@
 public class Task {
     private String taskName;
+    private boolean isDone;
 
     public Task(String taskName) {
         this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done:\n" + this);
+    }
+
+    public void unmark() {
+        this.isDone = false;
+        System.out.println("Nice! I've marked this task as not done yet:\n" + this);
     }
 
     @Override
     public String toString() {
-        return this.taskName;
+        if (this.isDone) {
+            return "[X] " + this.taskName;
+        } else {
+            return "[ ] " + this.taskName;
+        }
     }
 }
