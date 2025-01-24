d65a0a0e0e744dfc573593bcd91fcb21dc006cc8
lilyium <150597285+lilyium@users.noreply.github.com>
2025-01-24 14:18:46 +0800

    Complete Level 0

---
+++ src/main/java/Baron.java
@@ -0,0 +1,15 @@
+public class Baron {
+    private static String hello = "____________________________________________________________\n" +
+            "Hello! I'm Baron.\n" +
+            "What can I do for you?\n" +
+            "____________________________________________________________";
+
+    private static String bye = "Bye. Hope to see you again soon!\n" +
+            "____________________________________________________________";
+
+
+    public static void main(String[] args) {
+        System.out.println(hello);
+        System.out.println(bye);
+    }
+}
